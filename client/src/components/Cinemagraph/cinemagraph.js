a1438bdcfa9b57000233e9b06ab65b2423a4b272
kellidragon <dragonkelli@gmail.com>
2019-08-22 19:34:29 -0700

    cine centered

--- client/src/components/Cinemagraph/cinemagraph.js
+++ client/src/components/Cinemagraph/cinemagraph.js
@@ -10,10 +10,10 @@ const Cinemagraph = () => {
         <Col size="12">
         <div className= "containerIpad">
           <div className="imgDiv">
-          <img src={ipad} className="ipad img-responsive " alt="ipad" />
+          <img src={ipad} className="ipad " alt="ipad" />
           </div>
               <div className="cineDiv">
-                 <img src="https://joetowey.files.wordpress.com/2015/07/ferry-takeoff-landing.gif" frameBorder="0" allowFullScreen allow='autoplay'className="img-responsive" id="cinemagraph" alt="cine">
+                 <img src="https://joetowey.files.wordpress.com/2015/07/ferry-takeoff-landing.gif" frameBorder="0" allowFullScreen allow='autoplay'className="" id="cinemagraph" alt="cine">
                 </img> 
           </div>
         </div>
